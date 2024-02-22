3622101e5ad77d1a2f2cc2673579465e156045d8
Siamak Haschemi <siamak@i19s.com>
2024-02-22 15:51:34 +0100

    .

--- onenighttent/app.js
+++ onenighttent/app.js
@@ -1,7 +1,38 @@
 var map;
 
 function init() {
-    map = L.map('map', {doubleClickZoom: false}).locate({setView: true, maxZoom: 16});
+
+    let open_topo_map = L.tileLayer.provider('OpenTopoMap');
+    let osm_de = L.tileLayer.provider('OpenStreetMap.DE');
+    let osm_france = L.tileLayer.provider('OpenStreetMap.France');
+    let open_cycle_map = new L.TileLayer('https://tile.thunderforest.com/cycle/{z}/{x}/{y}{r}.png?apikey=db5ae1f5778a448ca662554581f283c5', {
+        maxZoom: 18
+    });
+
+
+    let baseMaps = {
+        "OpenTopoMap": open_topo_map,
+        "OpenStreetMap.DE": osm_de,
+        "OpenStreetMap.France": osm_france,
+        "OpenCycleMap": open_cycle_map
+    };
+
+    let overlays = {//add any overlays here
+
+    };
+
+    map = L.map('map', {
+        doubleClickZoom: false,
+        zoomControl: false
+    }).locate({setView: true, maxZoom: 18});
+
+    osm_france.addTo(map);
+
+    L.control.layers(baseMaps, overlays, {position: 'topleft'}).addTo(map);
+
+    L.control.zoom({
+        position: 'bottomright'
+    }).addTo(map);
 
     L.tileLayer('https://tile.thunderforest.com/atlas/{z}/{x}/{y}.png?apikey={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="http://www.thunderforest.com/">Thunderforest</a>',
@@ -9,6 +40,34 @@ function init() {
         id: 'thunderforest/atlas',
         accessToken: 'a794617134d14b1f82f1cd09d35bca51'
     }).addTo(map);
+
+    let scale = L.control.scale({position: 'topright'}); // Creating scale control
+    scale.addTo(map); // Adding scale control to the map
+
+    var locateBtn = L.control({position: 'bottomright'});
+    locateBtn.onAdd = function (map) {
+        var div = L.DomUtil.create('div', '');
+        div.innerHTML = '<button class="locate-btn">Locate Me</button>';
+        div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
+        L.DomEvent.on(div.firstChild, 'click', L.DomEvent.stop).on(div.firstChild, 'click', function () {
+            map.locate({setView: true, maxZoom: 16});
+        });
+        return div;
+    };
+    locateBtn.addTo(map);
+
+    const provider = new window.GeoSearch.OpenStreetMapProvider();
+    const search = new GeoSearch.GeoSearchControl({
+        provider: provider,
+        style: 'button',
+        updateMap: true,
+        autoClose: true,
+        autoComplete: true, // optional: true|false  - default true
+        autoCompleteDelay: 250, // optional: number      - default 250
+    }); // Include the search box with usefull params. Autoclose and updateMap in my case. Provider is a compulsory parameter.
+
+    map.addControl(search);
+
 
     // Fetch GeoJSON file
     fetch('./data/campgrounds.geojson')
@@ -20,28 +79,6 @@ function init() {
 
             map.on('locationfound', onLocationFound);
             map.on('locationerror', onLocationError);
-
-            var locateBtn = L.control({position: 'topright'});
-            locateBtn.onAdd = function (map) {
-                var div = L.DomUtil.create('div', '');
-                div.innerHTML = '<button class="locate-btn">Locate Me</button>';
-                div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
-                L.DomEvent.on(div.firstChild, 'click', L.DomEvent.stop).on(div.firstChild, 'click', function () {
-                    map.locate({setView: true, maxZoom: 16});
-                });
-                return div;
-            };
-            locateBtn.addTo(map);
-
-            const provider = new window.GeoSearch.OpenStreetMapProvider();
-            const search = new GeoSearch.GeoSearchControl({
-                provider: provider,
-                style: 'bar',
-                updateMap: true,
-                autoClose: true,
-            }); // Include the search box with usefull params. Autoclose and updateMap in my case. Provider is a compulsory parameter.
-
-            map.addControl(search);
         })
         .catch(function (error) {
             console.log('Error loading the GeoJSON file: ' + error.message);
